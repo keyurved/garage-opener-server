324f5bdec3c03bd530991743e3633fb4c5e37676
Keyur Ved <vedkeyur@gmail.com>
2019-06-30 20:34:17 -0400

    Add login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const garageRoutes = require('./garage');
 
 function construct(app) {
 
+    app.use('/login', () => {
+        res.json({'message': 'ok'});
+    });
     app.use('/garage', garageRoutes);
     app.use('*', (_req, res) => {
         res.status(404).json({'message': 'Not Found'});
